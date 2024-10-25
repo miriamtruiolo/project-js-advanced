7f4de4b33519b165844b8751be5cccc6984e077c
miriamtruiolo <miriamtruiolo@gmail.com>
2024-10-25 21:27:39 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader', // Per supportare ES6+
-        },
-      },
-      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'], // Per i file CSS
       },
